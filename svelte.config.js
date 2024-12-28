f3a65c671c929b0c0795782b3e30b2cb13f5d773
khmerweb <vuthdevelop1@gmail.com>
2024-12-29 01:54:51 +0700

    Make it better

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 //import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { mdsvex } from 'mdsvex'
 
 /** @type {import('@sveltejs/kit').Config} */
 
